7f79a66cdc8c7dbb38fa1f0151275d6532ab53f9
amittlem <amittlem@andrew.cmu.edu>
2021-12-03 00:45:11 -0500

    timer carousel with text

--- Assets/main.js
+++ Assets/main.js
@@ -2,9 +2,11 @@ function scrollCarousel(){
     $("#myCarousel").carousel("next");
 }
 
-$(window).scroll(function() {
-    if($(window).scrollTop() + $(window).height() == $(document).height()) {
-        scrollCarousel();
-        window.scrollTo(0, $(document).height()-$(window).height()-75);
-    }
- });
+// $(window).scroll(function() {
+//     if($(window).scrollTop() + $(window).height() == $(document).height()) {
+//         scrollCarousel();
+//         window.scrollTo(0, $(document).height()-$(window).height()-75);
+//     }
+//  });
+
+setInterval(scrollCarousel, 5000)
